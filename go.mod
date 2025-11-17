0f3dcd423c129c498e662e42e11f1d66076862d4
p.mostafayi <p.mostafayi@armaghan.net>
2025-11-17 10:19:27 +0330

    Add register user service

--- go.mod
+++ go.mod
@@ -9,7 +9,10 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.3.0
 	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.44.0
 )
+
+replace golang.org/x/crypto v0.44.0 => F:\libs\crypto
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
@@ -38,9 +41,9 @@ require (
 	go.opentelemetry.io/otel/trace v1.37.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
-	golang.org/x/net v0.43.0 // indirect
-	golang.org/x/sys v0.36.0 // indirect
-	golang.org/x/text v0.29.0 // indirect
+	golang.org/x/net v0.46.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/text v0.31.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250811230008-5f3141c8851a // indirect
 	google.golang.org/grpc v1.74.2 // indirect
 	google.golang.org/protobuf v1.36.7 // indirect
--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -1,8 +1,10 @@
 package service
 
 import (
+	"github.com/payam1986128/go-fiber-sms-firewall/internal/entity"
 	"github.com/payam1986128/go-fiber-sms-firewall/internal/presentation"
 	"github.com/payam1986128/go-fiber-sms-firewall/internal/repository"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserService struct {
@@ -16,7 +18,14 @@ func NewUserService(repository *repository.UserRepository) *UserService {
 }
 
 func (service *UserService) RegisterUser(request *presentation.RegisterUserRequest, code string) (string, error) {
-	return "", nil
+	hashed, _ := bcrypt.GenerateFromPassword([]byte(code), bcrypt.DefaultCost)
+	user := &entity.User{
+		Username: *request.Username,
+		Password: string(hashed),
+		Active:   true,
+	}
+	id, err := service.repository.AddUser(user)
+	return id.String(), err
 }
 
 func (service *UserService) LoginUser(username string, password string) (*presentation.VerificationResponse, error) {
